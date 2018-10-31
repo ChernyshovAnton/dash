4a915a419aaaf90beaea39d74d57ce7bf278e997
Philippe Duval <philippe@plot.ly>
2018-10-31 16:42:13 -0400

    Pylint fixes

--- dash/development/base_component.py
+++ dash/development/base_component.py
@@ -12,6 +12,7 @@ class ComponentRegistry(abc.ABCMeta):
 
     component_registry = set()
 
+    # pylint: disable=arguments-differ
     def __new__(mcs, name, bases, attributes):
         component = abc.ABCMeta.__new__(mcs, name, bases, attributes)
         if name == 'Component':
